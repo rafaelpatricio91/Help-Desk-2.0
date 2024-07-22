b799e436afa10c0d085d8da4e2e4e42b009d1421
Rafa Patricio <rafaelpatricio91@gmail.com>
2024-07-22 08:31:29 -0300

    feat: add user repository

---
+++ user-service-api/src/main/java/br/com/rafa/userserviceapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.rafa.userserviceapi.repository;
+
+
+import br.com.rafa.userserviceapi.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
